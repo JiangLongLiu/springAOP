d01ad4f0563546de882ec41dc63f4791d6f68c78
JiangLongLiu <757617012@qq.com>
2014-09-09 19:00:44 +0800

    Update PersonService.java

--- springAOP/src/com/liujl/service/PersonService.java
+++ springAOP/src/com/liujl/service/PersonService.java
@@ -1,5 +1,7 @@
 package com.liujl.service;
 
 public interface PersonService {
-	public abstract void save();
+	public abstract void save(String name);
+	public abstract String getPersonName(Integer id);
+	
 }
